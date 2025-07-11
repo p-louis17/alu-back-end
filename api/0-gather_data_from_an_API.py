62c4e993e6abaa20dc49f777d702e1d384033d67
p-louis17 <p.nsigo@alustudent.com>
2025-07-11 13:11:28 +0200

    Task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+getting data using api
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_Id = int(sys.argv[1])
+
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    user_data_url = "https://jsonplaceholder.typicode.com/users"
+
+    user_response = requests.get(user_data_url)
+    todo_response = requests.get(todo_url)
+
+    todos = todo_response.json()
+    users = user_response.json()
+    for user in users:
+        if user.get("id") == employee_Id:
+            employee_name = user.get("name")
+
+    done = []
+    total = 0
+    completed = 0
+    for todo in todos:
+        if todo.get("userId") == employee_Id:
+            total += 1
+            if todo.get("completed"):
+                completed += 1
+                done.append(todo.get("title"))
+
+    print(f"Employee {employee_name} is done with tasks({completed}/{total}):")
+    for _ in done:
+        print(f"\t {_}")
